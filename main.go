ae4a20f4fc6ae4f16b215ec4c12ea46497b6ac2f
RustyDoggoBytes <rustydoggobytes@gmail.com>
2024-02-15 06:55:08 -0700

    Update templ and use embed

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func main() {
+//go:embed static/*
+var embededFiles embed.FS
 
+func getFileSystem(useOS bool) http.FileSystem {
+	fsys, err := fs.Sub(embededFiles, "static")
+	if err != nil {
+		panic(err)
+	}
+
+	return http.FS(fsys)
+}
+
+func main() {
 	e := echo.New()
+
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
+
+	assetHandler := http.FileServer(getFileSystem(false))
+	e.GET("/static/*", echo.WrapHandler(http.StripPrefix("/static/", assetHandler)))
 
 	e.GET("/", func(c echo.Context) error {
 		component := layout(index())
