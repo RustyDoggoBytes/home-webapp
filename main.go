a9dd05c0eb3a8685160c9c09e0816ba534788432
RustyDoggoBytes <rustydoggobytes@gmail.com>
2024-03-14 16:09:46 -0600

    Add fileserver

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 //go:embed static/*
 var embededFiles embed.FS
 
-func getFileSystem(useOS bool) http.FileSystem {
+func getFileSystem() http.FileSystem {
 	fsys, err := fs.Sub(embededFiles, "static")
 	if err != nil {
 		panic(err)
@@ -27,9 +27,9 @@ func main() {
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
 
-	assetHandler := http.FileServer(getFileSystem(false))
-	e.GET("/static/*", echo.WrapHandler(http.StripPrefix("/static/", assetHandler)))
+	assetHandler := http.FileServer(getFileSystem())
 
+	e.GET("/static/*", echo.WrapHandler(http.StripPrefix("/static/", assetHandler)))
 	e.GET("/", func(c echo.Context) error {
 		component := layout(index())
 		return component.Render(c.Request().Context(), c.Response())
