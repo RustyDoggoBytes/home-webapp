57eb48cd6cee69cc838e2449c59449975511a21b
RustyDoggoBytes <rustydoggobytes@gmail.com>
2024-03-29 22:02:07 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@ APP=home
 
 templ generate
 GOOS=linux GOARCH=386 go build -o $APP
+ssh linode "sudo service $APP stop"
 scp $APP linode:/home/rustydoggobytes/apps/
+ssh linode "sudo service $APP start"
 
